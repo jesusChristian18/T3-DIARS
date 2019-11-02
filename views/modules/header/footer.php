0e420d12aa65ced5b992527aa204474f701b7a00
jesusChristian18 <chris.jesus1997@gmail.com>
2019-11-02 12:18:28 -0500

    T3 DIARS

---
+++ views/modules/header/footer.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-inverse bg-inverse">
+    <div class="container animated  bounceInRight">
+        <cite>
+            Tienda Juvenil Carloncho 2019
+        </cite>
+    </div>
+</nav>
